2f3c2d89397e7ab38eb4b215f293bac236f7a588
Aman Goyal <im.developer26@gmail.com>
2021-06-08 01:41:10 +0530

    FIX: product card

--- src/components/product-card/styles.js
+++ src/components/product-card/styles.js
@@ -7,18 +7,27 @@ const ProductCard = {
         display: flex;
         overflow: hidden;
         flex-direction: column;
+        justify-content: space-between;
         cursor: pointer;
         transition: ease-in-out 0.3s ease-in-out;
         border: solid 1px rgba(0, 0, 0, 0);
         border: solid 1px rgba(0, 0, 0, 0.2);
+        border-radius: 5px;
         :hover {
             border: solid 1px rgba(0, 0, 0, 0.2);
         }
+
         h3 {
-            font-size: 16px;
+            /* font-size: 14px; */
         }
         p {
-            font-size: 20px;
+            line-height: 20px;
+            text-transform: capitalize;
+
+            overflow: hidden;
+            display: -webkit-box;
+            -webkit-line-clamp: 2;
+            -webkit-box-orient: vertical;
         }
     `,
     Container: styled.div`
@@ -28,12 +37,13 @@ const ProductCard = {
     `,
     DetailContainer: styled.div`
         min-height: 50px;
+        height: 100%;
         text-align: left;
         width: 100%;
         display: flex;
         flex-direction: column;
         gap: 1rem;
-        justify-content: space-evenly;
+        justify-content: space-between;
         padding: 5px 0;
     `
 };
